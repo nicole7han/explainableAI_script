221dbdec85c51efe2a942005cff75bc69c3e4194
nicole7han <nicole7han@gmail.com>
2022-01-11 14:12:48 -0800

    update data

--- runExperiment_binary.py
+++ runExperiment_binary.py
@@ -12,7 +12,7 @@ import os, sys, glob, json
 
 # set up path
 current_path = os.path.abspath(os.getcwd())
-stim_path = "{}/Stimuli_old".format(current_path)
+stim_path = "{}/Stimuli".format(current_path)
 data_path = "{}/Data".format(current_path)
 script_path = current_path
 
@@ -49,9 +49,9 @@ else:
 
 
 #check subject previous blocks
-if os.path.isdir(data_path + os.sep + "Data"+ os.sep + 'subj' +expInfo['Participant'])==False:
-    os.mkdir(data_path + os.sep + "Data"+ os.sep + 'subj' +expInfo['Participant'])
-subj_files = glob.glob(data_path + os.sep + "Data" + os.sep + 'subj' + expInfo['Participant'] + os.sep +"*.xlsx")
+if os.path.isdir(data_path + os.sep + 'subj' +expInfo['Participant'])==False:
+    os.mkdir(data_path + os.sep + 'subj' +expInfo['Participant'])
+subj_files = glob.glob(data_path + os.sep + 'subj' + expInfo['Participant'] + os.sep +"*.xlsx")
 num_blocks = len(subj_files)
 continue_last = False
 finished_trials = []
@@ -73,7 +73,7 @@ else:
 expInfo['Block_num'] = block_num
 
 # Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
-filename = data_path + os.sep + u'data' + os.sep + 'subj' + expInfo['Participant'] + os.sep +'subj'+ '%s_%s_%s_%s' %(expInfo['Participant'], expInfo['Block_num'], expInfo['Age'], expInfo['Gender'])
+filename = data_path + os.sep + 'subj' + expInfo['Participant'] + os.sep +'subj'+ '%s_%s_%s_%s' %(expInfo['Participant'], expInfo['Block_num'], expInfo['Age'], expInfo['Gender'])
 
 endExpNow = False  # flag for 'escape' or other condition => quit the exp
 
